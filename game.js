3b6464b3ddefafcf0b7e5e05ea00e113ca415ebd
DBlatcher <Dv.bltchr@gmail.com>
2020-07-26 15:59:54 +0100

    streamline game

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ function initGame(alphabet) {
     var template = document.querySelector("#gameTemplate")
 
     var filterTemplate = document.querySelector("#filterTemplate")
-    var filters = document.querySelector('#filters')
+    
 
     var state= {
         character:null,
@@ -19,35 +19,30 @@ function initGame(alphabet) {
         return function(character){return true }
     } 
 
+    function handleNextButton() {
+        if (state.phase == 0) {return startGame()}
+        if (state.phase == 1) {return showAnswer()}
+        if (state.phase == 2) {return startGame()}
+    }
+
     function startGame() {
-        if (state.phase !== 0) {return reset()}
         state.character = alphabet.random(makefilter())
         holder.querySelector('[data-role=question]').innerHTML = state.character.html
+        holder.querySelector('[data-role=answer]').innerHTML = ""
         state.phase = 1
     }
 
     function showAnswer() {
-        if (state.phase !== 1) {return false}
         holder.querySelector('[data-role=answer]').innerHTML = state.character.phonetic
         state.phase = 2
     }
 
-    function reset() {
-        state.phase=0
-        state.character=null
-
-        while (holder.childElementCount > 0) {
-            holder.removeChild(holder.firstChild)
-        }
-        holder.appendChild(template.content.cloneNode(true))
-        holder.querySelector('[data-role=start-button]').addEventListener('click',startGame)
-        holder.querySelector('[data-role=answer-button]').addEventListener('click',showAnswer)
-        holder.querySelector('[data-role=restart-button]').addEventListener('click',reset)
-    }
 
     function makefilterButtons() {
 
-        function makefilterButton(constanent) {
+        var filters = holder.querySelector('.filters')
+
+        function makefilterButton(constanent, isChecked) {
             var node = filterTemplate.content.cloneNode(true)
             let nodeInput = node.querySelector('input');
             let nodeLabel = node.querySelector('label');
@@ -56,6 +51,7 @@ function initGame(alphabet) {
             nodeSpan.innerText = constanentDisplayValue
             nodeLabel.setAttribute('for', "filter-"+constanent)
             nodeInput.setAttribute('id', "filter-"+constanent)
+            nodeInput.checked = isChecked
             nodeInput.addEventListener('change', function(event){
                 var index = state.constanentFilter.indexOf(constanent)
                 if (this.checked && index === -1) {
@@ -70,12 +66,27 @@ function initGame(alphabet) {
         }
 
         alphabet.constanents.forEach(function (constanent) {
-            filters.appendChild(makefilterButton(constanent))
+            filters.appendChild(makefilterButton(constanent,state.constanentFilter.includes(constanent)))
         })
     }
 
-    makefilterButtons()
+
+    function reset() {
+        state.phase=0
+        state.character=null
+
+        while (holder.childElementCount > 0) {
+            holder.removeChild(holder.firstChild)
+        }
+        holder.appendChild(template.content.cloneNode(true))
+        holder.querySelector('[data-role=next-button]').addEventListener('click',handleNextButton)
+        holder.querySelector('[data-role=restart-button]').addEventListener('click',reset)
+
+        makefilterButtons()
+    }
+
     reset()
+    
     return state
 }
 
